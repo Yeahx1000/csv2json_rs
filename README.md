8c251d0eeab1a17b56551510c64995a4ebd964e3
Ekene <83151271+Yeahx1000@users.noreply.github.com>
2026-01-29 22:04:51 -0500

    - implement main.rs logic - fix `sample.csv` address column - update `README.md`

--- README.md
+++ README.md
@@ -18,3 +18,49 @@ CLI tool for converting CSV files to JSON arrays
 - Handles headers
 - Some error logging (bad file, bad CSV, parse issues)
 - modular structure, organized by concerns
+
+## Usage
+
+```bash
+csv2json-rs <input_file> [options]
+```
+
+so for example, in terminal to test you'd do:
+
+```bash
+cargo run --input_file samples/sample.csv --pretty --output-file output.json
+```
+
+so cargo run `<input_file_location>` `--option` (like `--pretty`) then `--output-file <path>`. you don't have to type flag `--input_file` or `--output-file` since clap automatically parses the arguments, added for examples and explanation.
+
+if no output is specified, it will default to stdout, which is the terminal.
+
+## Options
+
+```bash
+--pretty: Pretty-print the JSON output
+--output-file <path>: Write output to a file
+```
+
+## Examples
+
+```bash
+csv2json-rs samples/sample.csv --pretty
+csv2json-rs samples/sample.csv --output-file output.json
+```
+
+## Output
+
+```json
+[
+  {
+    "name": "John Doe",
+    "age": "30",
+    "address": "123 Main St Anytown USA"
+  }
+]
+```
+
+## License
+
+MIT License
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,58 @@
 // default main entry point
+use clap::Parser;
+use std::fs::File;
+use std::io::{self, Write};
+
+use csv2json_rs::convert::csv_to_json;
+use csv2json_rs::error::AppResult;
+
+#[derive(Parser, Debug)]
+#[command(
+    name = "csv2json-rs",
+    version,
+    about = "CLI tool for converting CSV files to JSON arrays"
+)]
+struct Args {
+    input_file: String,
+    #[arg(long)]
+    pretty: bool,
+    #[arg(long)]
+    output_file: Option<String>,
+}
 
 pub fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run() {
+        eprintln!("Error: {e}");
+        std::process::exit(1);
+    }
+}
+
+fn run() -> AppResult<()> {
+    let args = Args::parse();
+    let file = File::open(&args.input_file)?;
+    let json_array = csv_to_json(file)?;
+
+    // if pretty print flag used, proceeds with pretty print, else, compact print
+    let output = if args.pretty {
+        serde_json::to_string_pretty(&json_array)?
+    } else {
+        serde_json::to_string(&json_array)?
+    };
+
+    match args.output_file {
+        Some(path) => {
+            // Writing output JSON to a specified file
+            let mut file = File::create(&path)?;
+            file.write_all(output.as_bytes())?;
+            println!("JSON saved to {path}");
+        }
+        None => {
+            // Write output JSON to stdout (locks for efficiency with large outputs)
+            let mut stdout: io::StdoutLock<'_> = io::stdout().lock();
+            stdout.write_all(output.as_bytes())?;
+            stdout.write_all(b"\n")?;
+        }
+    }
+
+    Ok(())
 }
